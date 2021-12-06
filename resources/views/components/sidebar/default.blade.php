9bc1f2b2c6eb81d6ddba57f4b08f8883e01f083d
Aakash1919 <programmer.aakash@gmail.com>
2021-12-06 11:01:22 +0530

    Added authorize access

--- resources/views/components/sidebar/default.blade.php
+++ resources/views/components/sidebar/default.blade.php
@@ -17,13 +17,13 @@
                 <div class="menu-title">Dashboard</div>
             </a>
         </li>
-        {{-- <li>
+        <li>
             <a href="{{ route('google-authorize') }}">
                 <div class="parent-icon"><i class='bx bx-home-circle'></i>
                 </div>
                 <div class="menu-title">Authorize</div>
             </a>
-        </li> --}}
+        </li>
         <li>
             <a href="{{ route('keyword-planner') }}">
                 <div class="parent-icon"><i class='bx bx-home-circle'></i>
