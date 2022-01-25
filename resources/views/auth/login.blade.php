9f9f15bd73f1241c2ab8dc5b5412a32b210f018f
Aakash1919 <programmer.aakash@gmail.com>
2022-01-25 22:45:49 +0530

    Added final changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,38 +8,43 @@
                 <div class="card-header">{{ __('Welcome back!') }}</div>
 
                 <div class="card-body">
-<div class="hadder-login">
-    <h3>Log in to your account</h3>
-    <p>If you are already a member, please enter your login details in the form below.</p>
-</div>
+                    <div class="hadder-login">
+                        <h3>Log in to your account</h3>
+                        <p>If you are already a member, please enter your login details in the form below.</p>
+                    </div>
 
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row email-fild">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address')
+                                }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row pssword-fild">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password')
+                                }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -47,7 +52,8 @@
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{
+                                        old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
@@ -63,17 +69,17 @@
                                 </button>
                                 <a class="btn btn-warning" href="{{ route('register') }}">{{ __('Sign Up Free') }}</a>
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
-<div class="social-btn">
-    <h3>Connect with social network</h3>
-    <a href="#" class="fb"><img src="assets/images/facebook.png">Sign in with Facebook</a>
-    <a href="#" class="google"><img src="assets/images/google.png">Sign in with Google</a>
-</div>
+                        <div class="social-btn">
+                            <h3>Connect with social network</h3>
+                            <a href="#" class="fb"><img src="assets/images/facebook.png">Sign in with Facebook</a>
+                            <a href="#" class="google"><img src="assets/images/google.png">Sign in with Google</a>
+                        </div>
 
                     </form>
                 </div>
--- resources/views/user/keywordPlanner.blade.php
+++ resources/views/user/keywordPlanner.blade.php
@@ -2,170 +2,181 @@
 
 @section('content')
 <div class="page-wrapper">
-    <div class="page-content">
-        @if (!isset($refreshToken))
-            <div class="alert alert-warning border-0 bg-warning alert-dismissible fade show py-2">
-                <div class="d-flex align-items-center">
-                    <div class="font-35 text-dark"><i class="bx bx-info-circle"></i>
-                    </div>
-                    <div class="ms-3">
-                        <h6 class="mb-0 text-dark">Authentication Alets</h6>
-                        <div class="text-dark">If you want to access full tools, please connect with google <a href="{{ route('google-authorize') }}" class="btn btn-sm btn-primary mb-2">Connect</a></div>
-                    </div>
-                </div>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
-        @if (session('status'))
-            <div class="alert alert-daner border-0 bg-daner alert-dismissible fade show py-2">
-                <div class="d-flex align-items-center">
-                    <div class="font-35 text-dark"><i class="bx bx-info-circle"></i>
-                    </div>
-                    <div class="ms-3">
-                        <h6 class="mb-0 text-dark">Message</h6>
-                        <div class="text-dark"> {{ session('status') }}</div>
-                    </div>
-                </div>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
-
-<div class="hadder-row">
-<div class="container">  
-<div class="col-md-6"><span>Keyword Research <em><img src="assets/images/right.png"></em></span> 
-<button type="button" class="btn btn-primary stig" data-toggle="modal" data-target="#mb-trk">
-Settings</button></div>
-<div class="col-md-6">
-<div class="lt-udt">
-<p>You have 2 of 3 checks left today</p></div> </div>
-</div>
-</div>
-
-  <div class="keyword-saction-one research-keyword">
-  <div class="container">
-    <div class="keyword-box-one">
-      <div class="tab-saction tab-keword">
-        <div id="tab-1" class="tab-content current">
-          <div class="table-price">
-            <form class="keword-from">
-              <div class="-kewleft-side">
-                <label>Keyword</label>
-                <input type="text" required name="keyword" class="form-control" autofocus placeholder="Keyword">
-              </div>
-              <div class="-kewright-side">
-                <label>Analysis</label>
-                <div class="slect-erow">
-                <select name="action" class="form-control" style="padding-left: 8px;" placeholder="Similar keywords">
-                  <option value="similar_keywords" selected="">Similar keywords</option>
-                  <option value="questions">Questions</option>
-                  <option value="similar_searches">Related searches</option>
-                  <option value="autocomplete">Auto complete</option>
-                  <option value="related_keywords">Related terms</option>
-                </select>
-               </div>
-              </div>
-              <div class="-kewful-side">
-                <label>Search engine</label>
-                <input type="text" name="searchengine" list="engines" class="form-control" placeholder="Google.com">
-              </div>
-              <div class="btn-keyword">
-                <input class="btn btn-primary form-control" type="submit" value="Keyword Research">
-              </div>
-            </form>
-          </div>
+  <div class="page-content">
+    @if (!isset($refreshToken))
+    <div class="alert alert-warning border-0 bg-warning alert-dismissible fade show py-2">
+      <div class="d-flex align-items-center">
+        <div class="font-35 text-dark"><i class="bx bx-info-circle"></i>
         </div>
-        <div id="tab-2" class="tab-content">
-          <div class="table-price">
-            <form class="keword-from">
-              <div class="-kewful-side url-domain">
-                <label>URL / Domain</label>
-                <input class="form-control seocheckurlinput" type="text" required name="url" placeholder="example.com" autofocus>
-              </div>
-              <div class="-kewful-side url-domain">
-                <label>Search engine </label>
-                <input type="text" name="searchengine" list="engines" class="form-control" placeholder="Google.com">
-              </div>
-              <div class="btn-keyword">
-                <input class="btn btn-primary form-control" type="submit" value="Keyword Research">
-              </div>
-            </form>
-          </div>
+        <div class="ms-3">
+          <h6 class="mb-0 text-dark">Authentication Alets</h6>
+          <div class="text-dark">If you want to access full tools, please connect with google <a
+              href="{{ route('google-authorize') }}" class="btn btn-sm btn-primary mb-2">Connect</a></div>
         </div>
-        <div id="tab-3" class="tab-content">
-          <div class="table-price">
-            <form class="keword-from">
-              <div class="-kewful-side">
-                <label>Your domain</label>
-                <input class="form-control seocheckurlinput" type="text" required name="comparedomain" placeholder="example.com">
-              </div>
-              <div class="-kewleft-side">
-                <label>Domain A </label>
-                <input class="form-control seocheckurlinput" type="text" required name="domainA" placeholder="example.com">
-              </div>
-              <div class="-kewright-side">
-                <label>Domain B </label>
-                <input class="form-control seocheckurlinput" type="text" required name="domainA" placeholder="example.com">
-              </div>
-              <div class="-kewleft-side analysis">
-                <label>Search engine</label>
-                <input class="form-control seocheckurlinput" type="text" required name="domainA" placeholder="example.com">
-              </div>
-              <div class="-kewright-side analysis">
-                <label>Analysis</label>
-                <div class="flex">
-                  <li class="switcher active">
-                    <label class="flex clear-margin"> <a>
-                      <input type="radio" name="mode" value="gap" checked="">
-                      </a>&nbsp;Gap</label>
-                  </li>
-                  <li class="switcher" style="margin-left: 40px;">
-                    <label class="flex clear-margin"> <a>
-                      <input type="radio" name="mode" value="intersect">
-                      </a>&nbsp;Intersections</label>
-                  </li>
-                </div>
-              </div>
-              <div class="btn-keyword">
-                <input class="btn btn-primary form-control" type="submit" value="Keyword Research">
-              </div>
-            </form>
+      </div>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+    @if (session('status'))
+    <div class="alert alert-daner border-0 bg-daner alert-dismissible fade show py-2">
+      <div class="d-flex align-items-center">
+        <div class="font-35 text-dark"><i class="bx bx-info-circle"></i>
+        </div>
+        <div class="ms-3">
+          <h6 class="mb-0 text-dark">Message</h6>
+          <div class="text-dark"> {{ session('status') }}</div>
+        </div>
+      </div>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    <div class="hadder-row">
+      <div class="container">
+        <div class="col-md-6"><span>Keyword Research <em><img src="assets/images/right.png"></em></span>
+          <button type="button" class="btn btn-primary stig" data-toggle="modal" data-target="#mb-trk">
+            Settings</button>
+        </div>
+        <div class="col-md-6">
+          <div class="lt-udt">
+            <p>You have 2 of 3 checks left today</p>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-    
-        @if(isset($keywordResponse['status']) && $keywordResponse['status']==true)
-        <div class="card">
-    		<div class="card-body">
-    			<table class="table mb-0">
-    				<thead>
-    					<tr>
-    						<th scope="col">S.No</th>
-    						<th scope="col">Keyword</th>
-    						<th scope="col">Average Monthly Searches</th>
-    						<th scope="col">Competition</th>
-    						<th scope="col">CPC</th>
-    					</tr>
-    				</thead>
-    				<tbody>
-    				    @if(isset($keywordResponse['data']) && is_array($keywordResponse['data']))
-        				    @foreach($keywordResponse['data'] as $key => $value)
-        					<tr>
-        						<th scope="row">{{++$key}}</th>
-        						<td>{{ $value['keyword'] ?? '' }}</td>
-        						<td>{{ $value['searches'] ?? '' }}</td>
-        						<td>{{ $value['competition'] ?? '' }}</td>
-        						<td>${{ $value['cpc'] ?? '0' }}</td>
-        					</tr>
-        					@endforeach
-    					@endif;
-    				</tbody>
-    			</table>
-    		</div>
-    	</div>
-        @endif
+
+    <div class="keyword-saction-one research-keyword">
+      <div class="container">
+        <div class="keyword-box-one">
+          <div class="tab-saction tab-keword">
+            <div id="tab-1" class="tab-content current">
+              <div class="table-price">
+                <form action="{{route('keyword-planner-post')}}" method="post" class="keword-from">
+                  @csrf
+                  <div class="-kewleft-side">
+                    <label>Keyword</label>
+                    <input type="text" required name="keyword" class="form-control" autofocus placeholder="Keyword">
+                  </div>
+                  <div class="-kewright-side">
+                    <label>Analysis</label>
+                    <div class="slect-erow">
+                      <select name="action" class="form-control" style="padding-left: 8px;"
+                        placeholder="Similar keywords">
+                        <option value="similar_keywords" selected="">Similar keywords</option>
+                        <option value="questions">Questions</option>
+                        <option value="similar_searches">Related searches</option>
+                        <option value="autocomplete">Auto complete</option>
+                        <option value="related_keywords">Related terms</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="-kewful-side">
+                    <label>Search engine</label>
+                    <input type="text" name="url" list="engines" class="form-control" placeholder="Google.com">
+                  </div>
+                  <div class="btn-keyword">
+                    <input class="btn btn-primary form-control" type="submit" value="Keyword Research">
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div id="tab-2" class="tab-content">
+              <div class="table-price">
+                <form class="keword-from">
+                  <div class="-kewful-side url-domain">
+                    <label>URL / Domain</label>
+                    <input class="form-control seocheckurlinput" type="text" required name="url"
+                      placeholder="example.com" autofocus>
+                  </div>
+                  <div class="-kewful-side url-domain">
+                    <label>Search engine </label>
+                    <input type="text" name="searchengine" list="engines" class="form-control" placeholder="Google.com">
+                  </div>
+                  <div class="btn-keyword">
+                    <input class="btn btn-primary form-control" type="submit" value="Keyword Research">
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div id="tab-3" class="tab-content">
+              <div class="table-price">
+                <form class="keword-from">
+                  <div class="-kewful-side">
+                    <label>Your domain</label>
+                    <input class="form-control seocheckurlinput" type="text" required name="comparedomain"
+                      placeholder="example.com">
+                  </div>
+                  <div class="-kewleft-side">
+                    <label>Domain A </label>
+                    <input class="form-control seocheckurlinput" type="text" required name="domainA"
+                      placeholder="example.com">
+                  </div>
+                  <div class="-kewright-side">
+                    <label>Domain B </label>
+                    <input class="form-control seocheckurlinput" type="text" required name="domainA"
+                      placeholder="example.com">
+                  </div>
+                  <div class="-kewleft-side analysis">
+                    <label>Search engine</label>
+                    <input class="form-control seocheckurlinput" type="text" required name="domainA"
+                      placeholder="example.com">
+                  </div>
+                  <div class="-kewright-side analysis">
+                    <label>Analysis</label>
+                    <div class="flex">
+                      <li class="switcher active">
+                        <label class="flex clear-margin"> <a>
+                            <input type="radio" name="mode" value="gap" checked="">
+                          </a>&nbsp;Gap</label>
+                      </li>
+                      <li class="switcher" style="margin-left: 40px;">
+                        <label class="flex clear-margin"> <a>
+                            <input type="radio" name="mode" value="intersect">
+                          </a>&nbsp;Intersections</label>
+                      </li>
+                    </div>
+                  </div>
+                  <div class="btn-keyword">
+                    <input class="btn btn-primary form-control" type="submit" value="Keyword Research">
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+    @if(isset($keywordResponse['status']) && $keywordResponse['status']==true)
+    <div class="card">
+      <div class="card-body">
+        <table class="table mb-0">
+          <thead>
+            <tr>
+              <th scope="col">S.No</th>
+              <th scope="col">Keyword</th>
+              <th scope="col">Average Monthly Searches</th>
+              <th scope="col">Competition</th>
+              <th scope="col">CPC</th>
+            </tr>
+          </thead>
+          <tbody>
+            @if(isset($keywordResponse['data']) && is_array($keywordResponse['data']))
+            @foreach($keywordResponse['data'] as $key => $value)
+            <tr>
+              <th scope="row">{{++$key}}</th>
+              <td>{{ $value['keyword'] ?? '' }}</td>
+              <td>{{ $value['searches'] ?? '' }}</td>
+              <td>{{ $value['competition'] ?? '' }}</td>
+              <td>${{ $value['cpc'] ?? '0' }}</td>
+            </tr>
+            @endforeach
+            @endif;
+          </tbody>
+        </table>
+      </div>
+    </div>
+    @endif
+  </div>
 </div>
 @endsection
