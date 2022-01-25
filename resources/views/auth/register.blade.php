1d22e5d28305402a44c80a010d54676feda03f59
surajpixxelu <surajpixxelu@gmail.com>
2022-01-25 12:18:39 +0530

    name-fild

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="form-group row name-fild">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
@@ -29,7 +29,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mai-fild">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -43,7 +43,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row pas-fild">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -57,7 +57,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row confmpas-fild">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
